520d4bdd9c8098a884020c54e080fa6c00b70362
SamiAbar30 <samiabar30@gmail.com>
2023-12-14 13:35:17 -0800

    add advanced files task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ * Description: A C program that runs with printf function
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(l , quo, 59);
 	return (1);
 }
