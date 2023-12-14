63ab3e7005626e1da581a06a8daa233be8649fb6
SamiAbar30 <samiabar30@gmail.com>
2023-12-14 07:47:04 -0800

    fix files discription

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: A C program that rints with printf function
+ * Retuen : Always 0 (success)
+ */
 int main(void)
 {
-i	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
