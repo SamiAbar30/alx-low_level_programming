b04d2c968522601a274870a6a2ab65410da55c76
SamiAbar30 <samiabar30@gmail.com>
2023-12-14 07:01:25 -0800

    add c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that rints with put function
+ * Retuen : Always 0 (success)
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+
+}
