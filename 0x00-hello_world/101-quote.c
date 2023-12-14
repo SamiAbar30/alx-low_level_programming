5b4fd18e5f9a9cf8ae2ce54080300c02fe740872
SamiAbar30 <samiabar30@gmail.com>
2023-12-14 13:45:13 -0800

    fix advanced files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  * Description: A C program that runs with printf function
- * Return: Always 0 (success)
+ * Return: Always 1 (unseccess)
  */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(l , quo, 59);
+	write(l, quo, 59);
 	return (1);
 }
