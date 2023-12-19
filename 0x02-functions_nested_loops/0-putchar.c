7742bebfc69f9179716f22daf821faa048256ed1
SamiAbar30 <samiabar30@gmail.com>
2023-12-19 06:32:30 -0800

    prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include 'main.h'
+/**
+ * main - entry point
+ * Description: putchar
+ * Return: 0
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
+}
