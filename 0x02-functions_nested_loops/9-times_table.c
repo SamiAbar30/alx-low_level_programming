7d4b3d6c5f5ff1405e93c44b0f6c2468598b418d
SamiAbar30 <samiabar30@gmail.com>
2023-12-19 10:45:16 -0800

    add times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ *times_table - computes the absolute value of an integer
+ *Discription: prints the last digit of a number
+ *Return: return 1 if the params id lowercase or uppercaseoterwise return 0
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int result = i * j;
+
+			if (result > 9)
+				_putchar((result / 10) + '0');
+			else
+				_putchar(' ');
+			_putchar((result % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
