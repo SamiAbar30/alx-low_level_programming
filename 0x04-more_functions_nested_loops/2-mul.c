77b8231d4606f1409c55b7d76ffaea0abad76e0b
SamiAbar30 <samiabar30@gmail.com>
2023-12-21 02:54:46 -0800

    add 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: param.
+ * @b: param.
+ * Return: multiplies two integers result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
