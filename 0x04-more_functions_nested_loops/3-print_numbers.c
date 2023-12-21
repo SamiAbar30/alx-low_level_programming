058072cbbd93eae9b40bafcb71c85a9d10eeba23
SamiAbar30 <samiabar30@gmail.com>
2023-12-21 03:09:01 -0800

    add 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point.
+ * description: prints the numbers, from 0 to 9.
+ * Return: multiplies two integers result.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i >= '9'; i++)
+		_putchar(i)
+	_putchar('\n')
+	return (a * b);
+}
