e97f0e54306b52a555a7820d9595c1e71bec793f
SamiAbar30 <samiabar30@gmail.com>
2023-12-21 07:32:30 -0800

    add 5-more_numbers.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - entry point.
- * description: prints the numbers, from 0 to 9.
+ * @n: param.
  * Return: multiplies two integers result.
  */
 
@@ -10,7 +10,7 @@ void print_line(int n)
 {
 	int i;
 
-	if(n>0)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
