4aa2530fdaf2859e22230e14757c1bbe00603d49
SamiAbar30 <samiabar30@gmail.com>
2023-12-21 06:19:28 -0800

    add 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - entry point.
+ * print_most_numbers - entry point.
  * description: prints the numbers, from 0 to 9.
  * Return: multiplies two integers result.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != '2' || i != '4')
+		if (i != '2' && i != '4')
 			_putchar(i);
 	}
 	_putchar('\n');
